2e55186fdf72bbd2f5edcad731fe84868a048818
Greg Samuelson <grssam@gmail.com>
2021-02-04 16:09:24 -0800

    individual players seperated from partners

--- getTournamentDetails.js
+++ getTournamentDetails.js
@@ -33,7 +33,7 @@ const router = require('express').Router();
       let url = window.location.href
       let equalLocation = url.indexOf('=')
       let tournamentId = url.substring(equalLocation + 1)
-      
+
       for (let i = startRow; i < startRow + numOfTeams; i++) {
         let indvPlayer = {
           name: '',
@@ -59,10 +59,63 @@ const router = require('express').Router();
       }
       return [players, tournamentId]
     })
-    console.log(tourneyData)
+    let rawPlayerData = tourneyData[0]
+    // await playerDataPrep(rawPlayerData)
+    console.log(playerDataPrep(rawPlayerData))
+    console.log(tourneyData[1])
   }
 
-  await getTourneyData(2018)
+  async function playerDataPrep(array) {
+    let players = []
+    for (let i = 0; i < array.length; i++) {
+      let playerA = {
+        name: '',
+        playerId: '',
+        finish: '',
+        nationality: ''
+      }
+      let playerB = {
+        name: '',
+        playerId: '',
+        finish: '',
+        nationality: ''
+      }
+      let finish= parseInt(array[i].finish)
+      if (finish <= 4) {
+        finish = finish
+      } else if (finish <= 8) {
+        finish = 5
+      }
+      else if (finish <= 16) {
+        finish = 9
+      } else if (finish <= 24) {
+        finish = 17
+      }
+      else if (finish <= 32) {
+        finish = 25
+      } else if (finish <= 40) {
+        finish = 33
+      } else if (41 <= finish) {
+        finish = 41
+      }
+      playerA.name = array[i].name
+      playerA.Id = array[i].playerId
+      playerA.finish = finish
+      playerA.nationality = array[i].nationality
+      playerB.name = array[i].partner
+      playerB.Id = array[i].partnerId
+      playerB.finish = finish
+      playerB.nationality = array[i].nationality
+
+      players.push(playerA)
+      players.push(playerB)
+    }
+    return players
+  }
+  let year = 2020
+  for (let y = year; y >= 2000; y--) {
+  await getTourneyData(y)
+  }
 
   for (let tournamentId of bviIdArray) {
     let url = `http://bvbinfo.com/Tournament.asp?ID=${tournamentId}`
